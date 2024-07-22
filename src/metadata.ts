b634176333a7260fde507600f0530d5e17d02bfd
mareksuscak <marek.suscak@gmail.com>
2024-07-22 23:57:37 +0200

    Remove schema and metadata

--- src/metadata.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {};
-    return { "@nestjs/swagger": { "models": [[import("./item/dto/item.dto"), { "ItemDto": { _id: { required: true, type: () => require("mongoose").Types.ObjectId }, id: { required: true, type: () => String }, name: { required: true, type: () => String } } }], [import("./item/dto/item-create.dto"), { "CreateItemDto": { name: { required: true, type: () => String } }, "CreateItemInput": { name: { required: true, type: () => String }, parent: { required: false, type: () => String } } }], [import("./item/dto/item-update.dto"), { "UpdateItemDto": {}, "UpdateItemInput": {} }]], "controllers": [] }, "@nestjs/graphql": { "models": [] } };
-};
